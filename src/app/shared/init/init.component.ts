451e9212e4c023b2997a90de2ca67f0fd4eddde0
usern4353 <user33333333@mail.com>
2023-05-17 17:54:04 -0400

    with get

--- src/app/shared/init/init.component.ts
+++ src/app/shared/init/init.component.ts
@@ -11,18 +11,7 @@ import { ConvocatoriaFilterPipe } from './convocatoriafilterpipe';
 import { DataService } from 'src/app/core/data.service';
 import { HttpClient } from '@angular/common/http';
 
-const CONVOCATORIAS: Convocatoria[] = data;
-function search(text: string, pipe: PipeTransform): Convocatoria[] {
-  return CONVOCATORIAS.filter((convocatoria) => {
-    const term = text.toLowerCase();
-    return (
-      pipe.transform(convocatoria.entidad).includes(term) ||
-      convocatoria.entidad.toLowerCase().includes(term) ||
-      pipe.transform(convocatoria.objeto).includes(term) ||
-      convocatoria.objeto.toLowerCase().includes(term)
-    );
-  });
-}
+
 
 @Component({
   selector: 'app-init',
@@ -36,12 +25,27 @@ function search(text: string, pipe: PipeTransform): Convocatoria[] {
 export class InitComponent {
   convocatorias$: Observable<Convocatoria[]> | undefined;
   filter = new FormControl('', { nonNullable: true });
+  CONVOCATORIAS: Convocatoria[] = data;
 
   constructor(pipe: ConvocatoriaFilterPipe, private dataService: DataService) {
-    this.dataService.getJson().subscribe(data11 => { console.log("ultimo",data11); });
+    this.dataService.getJson().subscribe(data11 => {
+      console.log("ultimo", data11);
+      this.CONVOCATORIAS = data11;
+    });
     this.convocatorias$ = this.filter.valueChanges.pipe(
       startWith(''),
-      map((text) => search(text, pipe)),
+      map((text) => this.search(text, pipe)),
     );
+  }
+  public search(text: string, pipe: PipeTransform): Convocatoria[] {
+    return this.CONVOCATORIAS.filter((convocatoria) => {
+      const term = text.toLowerCase();
+      return (
+        pipe.transform(convocatoria.entidad).includes(term) ||
+        convocatoria.entidad.toLowerCase().includes(term) ||
+        pipe.transform(convocatoria.objeto).includes(term) ||
+        convocatoria.objeto.toLowerCase().includes(term)
+      );
+    });
   }
 }
