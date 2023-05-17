5083531573431bde44118c297c07241eac1c5f26
usern4353 <user33333333@mail.com>
2023-05-17 00:28:34 -0400

    add logic f convocatories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InitComponent } from './shared/init/init.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InitComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
